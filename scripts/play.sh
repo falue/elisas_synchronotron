f62011b07f37d2f69940ea20e6cbec139ec5b956
falue <info@fluescher.ch>
2023-06-03 17:52:27 +0200

    Make executable

--- scripts/play.sh
+++ scripts/play.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-sudo chgrp esc /tmp/processing
-sudo chown esc /tmp/processing
 #sudo ~/Applications/processing-4.1.2/processing-java --sketch=/home/esc/Applications/sketchbook/synchronotron --run
 sudo /home/esc/Applications/sketchbook/elisas_synchronotron/linux-arm/elisas_synchronotron

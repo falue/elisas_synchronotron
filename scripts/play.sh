5eabcdeb987cb8ad3869643325d303dfd3efdc60
falue <info@fluescher.ch>
2023-06-03 17:08:20 +0200

    update for new folder structure

--- scripts/play.sh
+++ scripts/play.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 sudo chgrp esc /tmp/processing
 sudo chown esc /tmp/processing
-sudo ~/Applications/processing-4.1.2/processing-java --sketch=/home/esc/Applications/sketchbook/synchronotron --run
+#sudo ~/Applications/processing-4.1.2/processing-java --sketch=/home/esc/Applications/sketchbook/synchronotron --run
+sudo /home/esc/Applications/sketchbook/elisas_synchronotron/linux-arm/elisas_synchronotron
--- scripts/update_and_play.sh
+++ scripts/update_and_play.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cd ~/Applications/sketchbook/synchronotron
+cd ~/Applications/sketchbook/elisas_synchronotron
 echo "Hold your horses. Reset local changes first, if any.."
 git reset --hard
 git clean -fxd
@@ -8,5 +8,6 @@ echo "Get updates if available.."
 git pull
 echo "Done."
 echo "Start Application with sketch."
-sudo ~/Applications/processing-4.1.2/processing-java --sketch=/home/esc/Applications/sketchbook/synchronotron --run
+#sudo ~/Applications/processing-4.1.2/processing-java --sketch=/home/esc/Applications/sketchbook/synchronotron --run
+sudo /home/esc/Applications/sketchbook/elisas_synchronotron/linux-arm/elisas_synchronotron
 echo "Done. Hold on.."

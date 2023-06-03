6178b6a0ba412408831d72649fc2891f08aa17c0
falue <info@fluescher.ch>
2023-06-03 18:02:04 +0200

    Workaround: If processing-io lib is deleted with fresh deploy, auto add symlink when  git gets updated

--- scripts/update_and_play.sh
+++ scripts/update_and_play.sh
@@ -8,6 +8,10 @@ echo "Get updates if available.."
 git pull
 echo "Done."
 echo "Start Application with sketch."
+
+#if missing, make new symlink to  processing-io library
+ln -s ~/Applications/processing-4.1.2/modes/java/libraries/io/library/linux-armv6hf/libprocessing-io.so ~/Applications/sketchbook/elisas_synchronotron/linux-arm/lib/
+
 #sudo ~/Applications/processing-4.1.2/processing-java --sketch=/home/esc/Applications/sketchbook/synchronotron --run
 sudo /home/esc/Applications/sketchbook/elisas_synchronotron/linux-arm/elisas_synchronotron
 echo "Done. Hold on.."

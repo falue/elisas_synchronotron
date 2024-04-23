a36bd9a88dbdc05e0a7c94b5e2a8738c2dbf5cf1
falue <info@fluescher.ch>
2024-04-23 12:47:59 +0200

    remove unusable files

--- scripts/play.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-#sudo ~/Applications/processing-4.1.2/processing-java --sketch=/home/esc/Applications/sketchbook/synchronotron --run
-sudo /home/esc/Applications/sketchbook/elisas_synchronotron/linux-arm/elisas_synchronotron
--- scripts/update.sh
+++ scripts/update.sh
@@ -7,11 +7,11 @@ echo "Done."
 echo "Get updates if available.."
 git pull
 echo "Done."
-echo "Start Application with sketch."
 
 #if missing, make new symlink to  processing-io library
 ln -s ~/Applications/processing-4.1.2/modes/java/libraries/io/library/linux-armv6hf/libprocessing-io.so ~/Applications/sketchbook/elisas_synchronotron/linux-arm/lib/
 
-#sudo ~/Applications/processing-4.1.2/processing-java --sketch=/home/esc/Applications/sketchbook/synchronotron --run
-sudo /home/esc/Applications/sketchbook/elisas_synchronotron/linux-arm/elisas_synchronotron
-echo "Done. Hold on.."
+# Use Zenity to ask user to cancel shutdown
+if zenity --info --title="Elisa was updated" --text="Successful update. Start application a new."; then
+    echo "User acknowledged the message."
+fi

ad8a7858d149fe1da440594e83a4bba10c91b256
falue <info@fluescher.ch>
2024-04-23 20:10:21 +0200

    Revert "invert message logic"
    This reverts commit 7493fa79f32a70b98f270c53a4919fb554be2a82.

--- scripts/play_graceful_shutdown_arm.sh
+++ scripts/play_graceful_shutdown_arm.sh
@@ -7,9 +7,9 @@ sudo /home/esc/Applications/sketchbook/elisas_synchronotron/linux-arm/elisas_syn
 # shutdown after elisas_synchronotron has exit()ed
 
 # Use Zenity to ask user to cancel shutdown
-if zenity --question --title="Shutdown now?" --text="Press Yes or wait to shutdown now; or No to stop the shutdown." --timeout=7; then
+if zenity --question --title="Abort shutdown" --text="Press No or wait to shutdown now; or Yes to stop the shutdown." --timeout=5; then
+    echo "Shutdown canceled."
+else
     echo "Shutting down now..."
     shutdown -h now
-else
-    echo "Shutdown canceled."
 fi
